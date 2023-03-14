6e9a422fa4b39d73148398d571c19539b45245c8
jeffwang987 <jeffwang987@163.com>
2023-03-15 00:09:34 +0800

    fix import

--- projects/occ_plugin/datasets/pipelines/__init__.py
+++ projects/occ_plugin/datasets/pipelines/__init__.py
@@ -1,10 +1,10 @@
 from .transform_3d import (
     PadMultiViewImage, NormalizeMultiviewImage, 
     PhotoMetricDistortionMultiViewImage, CustomCollect3D, CustomOccCollect3D, RandomScaleImageMultiViewImage)
-from .formating import CustomDefaultFormatBundle3D, OccDefaultFormatBundle3D
+from .formating import OccDefaultFormatBundle3D
 from .loading import LoadOccupancy
 from .loading_bevdet import LoadAnnotationsBEVDepth, LoadMultiViewImageFromFiles_BEVDet
 __all__ = [
-    'PadMultiViewImage', 'NormalizeMultiviewImage',
-    'PhotoMetricDistortionMultiViewImage', 'CustomDefaultFormatBundle3D', 'CustomCollect3D', 'RandomScaleImageMultiViewImage',
+    'PadMultiViewImage', 'NormalizeMultiviewImage', 'CustomOccCollect3D', 'LoadAnnotationsBEVDepth', 'LoadMultiViewImageFromFiles_BEVDet', 'LoadOccupancy',
+    'PhotoMetricDistortionMultiViewImage', 'OccDefaultFormatBundle3D', 'CustomCollect3D', 'RandomScaleImageMultiViewImage',
 ]
